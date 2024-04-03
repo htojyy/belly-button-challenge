9db89ba32ab9be5b90b5272cd80c411d1bd7f0e8
htojyy <wyk27@duck.com>
2024-04-03 22:14:14 +1000

    add images folder, update readme, update init function in JavaScript

--- README.md
+++ README.md
@@ -1,13 +1,29 @@
 # belly-button-challenge
 
-* Use the D3 library to read in samples.json from the URL <https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json>.
-* Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-* Create a bubble chart that displays each sample.
-* Display each key-value pair from the metadata JSON object on the dashboard
-* Deploy app to a free static page hosting service, GitHub Pages
+## Objective
 
-## Results and Analysis
+Call JavaScript code from within HTML to create a site with interactive charts exploring the belly button biodiversity dataset (microbes that colonise human navels)
+
+## Method and Results
+
+* Use the D3 library to read in `samples.json` from the URL `https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json`
+<br><br>
+* Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual  
+<img src="Images/barchart.png" alt="Bar chart of top 10 sample IDs by count" width="300"><br><br>
+* Create a bubble chart that displays all samples  
+<img src="Images/bubbleplot.png" alt="Bubble plot of sample IDs and count of samples" width="500"><br><br>
+* Display each key-value pair from the metadata JSON object on the dashboard in the Demographic Info card  
+<img src="Images/demographicscard.png" alt="Demographics information card" width="100"><br><br>
+* Create an interactive dashboard that updates upon user selection of test subject ID number  
+<img src="Images/dashboard.png" alt="Dashboard with different test subject ID" width="600"><br><br>
+* Deploy app to a free static page hosting service on GitHub Pages: `https://htojyy.github.io/belly-button-challenge/`
 
 ## References
 
 * Hulcr, J. et al. (2012) A Jungle in There: Bacteria in Belly Buttons are Highly Diverse, but Predictable. Retrieved from: <http://robdunnlab.com/projects/belly-button-biodiversity/results-and-data/>
+* [Use array to create dropdown list text](https://www.quora.com/How-do-you-create-a-drop-down-list-in-JavaScript-using-an-array-as-the-data-source)
+* [Set default dropdown option](https://www.flowradar.com/answer/webflow-setting-default-option-select-dropdown-button-using-basic-javascript#:~:text=options%5B0%5D%60%20selects%20the,selection%20when%20the%20page%20loads)
+* [Check if value exists in array index](https://www.geeksforgeeks.org/how-to-check-a-value-exist-at-certain-array-index-in-javascript/)
+* [Plotly colour scales](//https://plotly.com/javascript/colorscales/)
+* [Get the container element and create a new element](https://www.linkedin.com/pulse/javascript-create-interactive-elements-within-your-page-svekis-#:~:text=createElement()%20method%20in%20JavaScript,creates%20a%20new%20div%20element)
+* [Get a key value pair from object](https://stackoverflow.com/questions/11279093/how-to-get-a-listing-of-key-value-pairs-in-an-object)
--- static/js/app.js
+++ static/js/app.js
@@ -1,155 +1,16 @@
 // Get the samples endpoint
 const url = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json";
 const samples = d3.json(url);
-
-// Initialises the page with a default plot
-function init() {
-
-// Use subject ID 959 as initial plot
-// bar chart
-    let x = [150,147,16,10,6,3,2,2,2,2].reverse();
-    let y = ['OTU 1795','OTU 922','OTU 943','OTU 936','OTU 924','OTU 3544','OTU 659','OTU 763','OTU 1054','OTU 1681'].reverse();
-    let hover = ["Bacteria;Firmicutes;Bacilli;Bacillales;Staphylococcaceae;Staphylococcus","Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Corynebacteriaceae;Corynebacterium","Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Corynebacteriaceae;Corynebacterium","Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Corynebacteriaceae;Corynebacterium","Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Corynebacteriaceae;Corynebacterium","Bacteria;Proteobacteria;Gammaproteobacteria;Pseudomonadales;Moraxellaceae;Acinetobacter","Bacteria;Actinobacteria;Actinobacteria;Actinomycetales","Bacteria;Actinobacteria;Actinobacteria;Actinomycetales","Bacteria;Actinobacteria;Actinobacteria;Bifidobacteriales;Bifidobacteriaceae","Bacteria;Firmicutes;Bacilli"];
-
-    console.log(x);
-    console.log(y);
-    console.log(hover);
-
-    let plotData = [{
-        y: y,
-        x: x,
-        type: 'bar',
-        orientation: 'h',
-        width: 0.8,
-        text: hover,
-        hovermode:'closest',
-        sort: false
-    }];
-
-    let layout = {
-        title: {
-            text: `Top 10 sample IDs by sample count<br>of selected subject ID`,
-            font: {
-                family: 'Arial',
-                size: 18
-            },
-            xref: 'paper',
-            x: 0.05,
-            automargin: true
-        },
-        xaxis: {
-            title: {
-                text: 'Count',
-                font: {
-                        family: 'Arial',
-                        size: 14,
-                        color: '#7f7f7f'
-                }
-            },
-        },
-        yaxis: {
-            title: {
-                text: 'Sample ID',
-                font: {
-                        family: 'Arial',
-                        size: 14,
-                        color: '#7f7f7f'
-                }
-            }
-        }
-      };
-    
-    Plotly.newPlot("bar", plotData, layout);
-
-// bubble chart
-    let yBubble = [150,147,16,10,6,3,2,2,2,2,2,2,2];
-    let xBubble = [1795,922,943,936,924,3544,659,763,1054,1681,1792,1794,3549];
-    let hoverBubble = ["Bacteria;Firmicutes;Bacilli;Bacillales;Staphylococcaceae;Staphylococcus","Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Corynebacteriaceae;Corynebacterium","Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Corynebacteriaceae;Corynebacterium","Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Corynebacteriaceae;Corynebacterium","Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Corynebacteriaceae;Corynebacterium","Bacteria;Proteobacteria;Gammaproteobacteria;Pseudomonadales;Moraxellaceae;Acinetobacter","Bacteria;Actinobacteria;Actinobacteria;Actinomycetales","Bacteria;Actinobacteria;Actinobacteria;Actinomycetales","Bacteria;Actinobacteria;Actinobacteria;Bifidobacteriales;Bifidobacteriaceae","Bacteria;Firmicutes;Bacilli","Bacteria;Firmicutes;Bacilli;Bacillales;Staphylococcaceae;Staphylococcus","Bacteria;Firmicutes;Bacilli;Bacillales;Staphylococcaceae;Staphylococcus","Bacteria;Proteobacteria;Gammaproteobacteria;Pseudomonadales;Moraxellaceae;Acinetobacter"];
-
-    console.log(xBubble);
-    console.log(yBubble);
-    console.log(hoverBubble);
-
-    let plotData2 = [{
-        y: yBubble,
-        x: xBubble,
-        type: 'bubble',
-        text: hoverBubble,
-        hovermode:'closest',
-        sort: false,
-        mode: 'markers',
-        marker: {
-            size: yBubble,
-            color: xBubble,
-            colorscale: 'Portland' //https://plotly.com/javascript/colorscales/
-        }
-    }];
-      
-    var layout2 = {
-        title: 'Total count of samples by sample ID for the selected individual',
-        showlegend: false,
-        height: 600,
-        width: 1200,
-        xaxis: {
-            title: {
-                text: 'Sample ID',
-                font: {
-                        family: 'Arial',
-                        size: 14,
-                        color: '#7f7f7f'
-                }
-            },
-        },
-        yaxis: {
-            title: {
-                text: 'Count',
-                font: {
-                        family: 'Arial',
-                        size: 14,
-                        color: '#7f7f7f'
-                }
-            }
-        }
-    };
-      
-    Plotly.newPlot('bubble', plotData2, layout2);
-
-// metadata - demographic info "sample-metadata"
-    
-    tableData = [{  "id": 959,
-                    "ethnicity": "Caucasian",
-                    "gender": "M",
-                    "age": 40.0,
-                    "location": "Carrboro/NC",
-                    "bbtype": "I",
-                    "wfreq": 5.0
-    }];
-
-    // Get the container element where the table will be inserted
-    // https://www.linkedin.com/pulse/javascript-create-interactive-elements-within-your-page-svekis-#:~:text=createElement()%20method%20in%20JavaScript,creates%20a%20new%20div%20element
-    // https://stackoverflow.com/questions/11279093/how-to-get-a-listing-of-key-value-pairs-in-an-object
-
-    let container = document.getElementById("sample-metadata");
-    // counter = 0;
-    for(var i in tableData[0]) {
-        console.log( i + ": " + tableData[0][i]);
-
-        // counter += 1;
-        var textToPrint = document.createElement("p");
-        textToPrint.innerHTML = i + ": " + tableData[0][i] + "<br>";
-        container.appendChild(textToPrint);
-        textToPrint.style.fontSize = "medium";
-    };
-    // Clear the options so can new elements p later - up to here?
-    textToPrint = [];
-};
-    
+const defaultIndex = 1;
 
 // Store all subject IDs in dropdown list, then update all plots on dropdown list value change
 // https://www.quora.com/How-do-you-create-a-drop-down-list-in-JavaScript-using-an-array-as-the-data-source
 samples.then(function(data) {
+
+    // store all subject IDs into a variable to fill dropdown menu
     let subjectNameID = data.names;
     console.log(subjectNameID);
+
     var select = document.getElementById("selDataset")
 
     for (var i =0; i < subjectNameID.length; i++) {
@@ -159,9 +20,9 @@ samples.then(function(data) {
         select.appendChild(option);
     };
 
-    // default drop down option
-    // https://www.flowradar.com/answer/webflow-setting-default-option-select-dropdown-button-using-basic-javascript#:~:text=options%5B0%5D%60%20selects%20the,selection%20when%20the%20page%20loads.
-    var defaultOption = select.options[16];
+    // Set default dropdown option
+    // https://www.flowradar.com/answer/webflow-setting-default-option-select-dropdown-button-using-basic-javascript#:~:text=options%5B0%5D%60%20selects%20the,selection%20when%20the%20page%20loads
+    var defaultOption = select.options[defaultIndex];
 
     defaultOption.selected = true;
 
@@ -170,6 +31,7 @@ samples.then(function(data) {
 
     // Function called by DOM changes
     function getData() {
+
         // Use D3 to select the dropdown menu
         let dropdownMenu = d3.select(this);
 
@@ -179,13 +41,13 @@ samples.then(function(data) {
         let index = subjectNameID.indexOf(`${dropDownChoice}`);
         console.log(`Index in array: ${index}`);
 
-    // =================================================================================================================
-    // bar chart
+        // =================================================================================================================
+        // bar chart
         // set empty arrays
         let labels = [];
         let x = [];
         let y = [];
-  
+
         // top 10 sample IDs only
         for (let j = 0; j < 10; j++) {
 
@@ -214,11 +76,11 @@ samples.then(function(data) {
         console.log(`x axis array: ${x}`);
         console.log(`labels for hover array: ${labels}`);
 
-        // Call function to update the chart
+        // Call function to update bar chart
         updatePlotlyBar(x.reverse(), y.reverse(), labels);
 
-    // =================================================================================================================
-    // bubble chart
+        // =================================================================================================================
+        // bubble chart
 
         // set empty arrays
         let labelsBubble = [];
@@ -230,13 +92,13 @@ samples.then(function(data) {
             // Get y axis (sample IDs) and save to array only if value exists in array index
             let otuID = data.samples[index]['otu_ids'][k];
                 if (typeof data.samples[index]['otu_ids'][k] !== "undefined") {
-                    yBubble.push(otuID);
+                    xBubble.push(otuID);
                 }
 
             // Get x axis (sample values) and save to array
             let values = data.samples[index]['sample_values'][k];
             if (typeof data.samples[index]['sample_values'][k] !== "undefined") {
-                xBubble.push(values);
+                yBubble.push(values);
             }
 
             // Get otu labels and save to array for hover
@@ -254,24 +116,22 @@ samples.then(function(data) {
         // Call function to update the bubble chart
         updatePlotlyBubble(xBubble, yBubble, labelsBubble);
 
-    // up to here
-    // Add demographic info. Clear the options then add new elements p again?
-    var paragraphs = document.getElementById("sample-metadata").getElementsByTagName("p");
-    var keys = Object.keys(data.metadata[index]);
-    var values = Object.values(data.metadata[index]);
 
-    console.log(keys);
-    console.log(values);
-    console.log(paragraphs);
-    console.log(index);
+        // Add demographic info - replace paragraph text
+        var paragraphs = document.getElementById("sample-metadata").getElementsByTagName("p");
+        var keys = Object.keys(data.metadata[index]);
+        var values = Object.values(data.metadata[index]);
 
-    for (var i = 0; i < paragraphs.length; i++) {
-        // d3.select(paragraphs[i]).text(keys[i] + ": " + values[i]);
-        paragraphs[i].textContent = keys[i] + ": " + values[i];
-    }
+        console.log(keys);
+        console.log(values);
+        console.log(paragraphs);
+        console.log(index);
 
+        for (var i = 0; i < paragraphs.length; i++) {
+            paragraphs[i].textContent = keys[i] + ": " + values[i];
+        }
 
-}
+    };
 
     // Update the bar chart with selected ID's values
     function updatePlotlyBar(x,y,labels) {
@@ -282,7 +142,7 @@ samples.then(function(data) {
     };
 
     // Update the bubble chart with selected ID's values
-    function updatePlotlyBubble(yBubble,xBubble,labelsBubble) {
+    function updatePlotlyBubble(xBubble,yBubble,labelsBubble) {
         Plotly.restyle("bubble", "x", [xBubble]);
         Plotly.restyle("bubble", "y", [yBubble]);
         Plotly.restyle("bubble", "text", [labelsBubble]);
@@ -290,17 +150,209 @@ samples.then(function(data) {
         Plotly.restyle("bubble", "marker.color", [xBubble]);      
     };
 
-
-
     // =================================================================================================================
     // reset arrays
     labels = [];
     x = [];
     y = [];
-    textToPrint = [];
-
 
 });
 
 
+
+// Initialises the page with a default plot
+function init() {
+
+    // Fetch data asynchronously from an external JSON file using D3.js
+    samples.then((data) => {
+
+        // bar chart
+        // set empty arrays
+        let labelsBar = [];
+        let xBar = [];
+        let yBar = [];
+
+        // top 10 sample IDs only of 2nd index
+        for (let j = 0; j < 10; j++) {
+            let otuID = data.samples[defaultIndex]['otu_ids'][j];
+
+            // Get y axis (sample IDs) and save to array only if value exists in array index
+            if (typeof data.samples[defaultIndex]['otu_ids'][j] !== "undefined") {
+                yBar.push(`OTU ${otuID}`);
+            }
+
+            // Get x axis (sample values) and save to array
+            let values = data.samples[defaultIndex]['sample_values'][j];
+            if (typeof data.samples[defaultIndex]['sample_values'][j] !== "undefined") {
+                xBar.push(values);
+            }
+
+            // Get otu labels and save to array for hover
+            let otu_labels = data.samples[defaultIndex]['otu_labels'][j];
+            if (typeof data.samples[defaultIndex]['otu_labels'][j] !== "undefined") {
+                labelsBar.push(otu_labels);
+            }
+
+        };
+
+        // Check values
+        console.log(xBar);
+        console.log(yBar);
+        console.log(labelsBar);
+
+        // Set bar chart data
+        let plotData = [{
+            y: yBar.reverse(),
+            x: xBar.reverse(),
+            type: 'bar',
+            orientation: 'h',
+            width: 0.8,
+            text: labelsBar.reverse(),
+            hovermode:'closest',
+            sort: false
+        }];
+
+        // Set bar chart layout
+        let layout = {
+            title: {
+                text: `Top 10 sample IDs by sample count<br>of selected subject ID`,
+                font: {
+                    family: 'Arial',
+                    size: 18
+                },
+                xref: 'paper',
+                x: 0.05,
+                automargin: true
+            },
+            xaxis: {
+                title: {
+                    text: 'Count',
+                    font: {
+                            family: 'Arial',
+                            size: 14,
+                            color: '#7f7f7f'
+                    }
+                },
+            },
+            yaxis: {
+                title: {
+                    text: 'Sample ID',
+                    font: {
+                            family: 'Arial',
+                            size: 14,
+                            color: '#7f7f7f'
+                    }
+                }
+            }
+        };
+    
+        // Plot bar chart
+        Plotly.newPlot("bar", plotData, layout);
+
+    
+    
+        // bubble chart
+
+        // set empty arrays
+        let labelsBubble = [];
+        let xBubble = [];
+        let yBubble = [];
+
+        for (let k = 0; k < data.samples[defaultIndex]['otu_ids'].length; k++) {
+
+            // Get y axis (sample IDs) and save to array only if value exists in array index
+            let otuID = data.samples[defaultIndex]['otu_ids'][k];
+                if (typeof data.samples[defaultIndex]['otu_ids'][k] !== "undefined") {
+                    xBubble.push(otuID);
+                }
+
+            // Get x axis (sample values) and save to array
+            let values = data.samples[defaultIndex]['sample_values'][k];
+            if (typeof data.samples[defaultIndex]['sample_values'][k] !== "undefined") {
+                yBubble.push(values);
+            }
+
+            // Get otu labels and save to array for hover
+            let otu_labels = data.samples[defaultIndex]['otu_labels'][k];
+            if (typeof data.samples[defaultIndex]['otu_labels'][k] !== "undefined") {
+                labelsBubble.push(otu_labels);
+            }
+        };
+
+        // print arrays to console
+        console.log(`y axis array: ${yBubble}`);
+        console.log(`x axis array: ${xBubble}`);
+        console.log(`labels for hover array: ${labelsBubble}`);
+            
+        // Set bubble plot data
+        let plotData2 = [{
+            y: yBubble,
+            x: xBubble,
+            type: 'bubble',
+            text: labelsBubble,
+            hovermode:'closest',
+            sort: false,
+            mode: 'markers',
+            marker: {
+                size: yBubble,
+                color: xBubble,
+                colorscale: 'Portland' //https://plotly.com/javascript/colorscales/
+            }
+        }];
+
+        // Set bubble plot layout  
+        var layout2 = {
+            title: 'Total count of samples by sample ID for the selected individual',
+            showlegend: false,
+            height: 600,
+            width: 1200,
+            xaxis: {
+                title: {
+                    text: 'Sample ID',
+                    font: {
+                            family: 'Arial',
+                            size: 14,
+                            color: '#7f7f7f'
+                    }
+                },
+            },
+            yaxis: {
+                title: {
+                    text: 'Count',
+                    font: {
+                            family: 'Arial',
+                            size: 14,
+                            color: '#7f7f7f'
+                    }
+                }
+            }
+        };
+         
+        //Plot bubble plot
+        Plotly.newPlot('bubble', plotData2, layout2);
+    
+        // Add demographic info card
+        // Get the container element where the table will be inserted
+        // https://www.linkedin.com/pulse/javascript-create-interactive-elements-within-your-page-svekis-#:~:text=createElement()%20method%20in%20JavaScript,creates%20a%20new%20div%20element
+        // https://stackoverflow.com/questions/11279093/how-to-get-a-listing-of-key-value-pairs-in-an-object
+
+        let container = document.getElementById("sample-metadata");
+
+        var keys = Object.keys(data.metadata[defaultIndex]);
+        var valuesTbl = Object.values(data.metadata[defaultIndex]);
+
+        console.log(keys);
+        console.log(valuesTbl);
+
+        for (var i = 0; i < keys.length; i++) {
+            var textToPrint = document.createElement("p");
+            textToPrint.innerHTML = keys[i] + ": " + valuesTbl[i];
+            container.appendChild(textToPrint);
+            textToPrint.style.fontSize = "medium";
+        };
+    
+    })
+};
+
+// initialise default bar plot, bubble plot, demographics card
 init();
